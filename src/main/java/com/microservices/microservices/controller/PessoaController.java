37b7fa4f09fdcc34d02c0caf80679c81d586f06a
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-13 20:07:13 -0300

    Rename alterar method to editar in PessoaController; streamline response messages and remove unnecessary comments.

--- src/main/java/com/microservices/microservices/controller/PessoaController.java
+++ src/main/java/com/microservices/microservices/controller/PessoaController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
@@ -44,20 +43,12 @@ public class PessoaController {
     }
 
     @PutMapping("/editar/{id}")
-    public ResponseEntity<String> alterar(@PathVariable int id, @RequestBody Pessoa pessoaAtualizada) {
-
-        // Verifica se a pessoa com o ID informado existe no banco de dados
+    public ResponseEntity<String> editar(@PathVariable int id, @RequestBody Pessoa pessoaAtualizada) {
         if (repository.existsById(id)) {
-
-            // Garante que o objeto atualizado tenha o ID correto antes de salvar
             pessoaAtualizada.setId(id);
-
-            // O método save do Spring Data JPA atualiza o registro se o ID já existir
             repository.save(pessoaAtualizada);
-
             return new ResponseEntity<>("A pessoa com o ID " + id + " foi alterada com sucesso!", HttpStatus.OK);
         } else {
-            // Retorna erro 404 caso o ID não seja encontrado
             return new ResponseEntity<>("Pessoa não encontrada.", HttpStatus.NOT_FOUND);
         }
     }
