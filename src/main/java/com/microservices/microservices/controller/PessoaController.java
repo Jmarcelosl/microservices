64dcef11ff037248ed9fd127714f477b5d8f64f9
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-13 20:29:00 -0300

    Remove unused imports in PessoaController to clean up the code

--- src/main/java/com/microservices/microservices/controller/PessoaController.java
+++ src/main/java/com/microservices/microservices/controller/PessoaController.java
@@ -3,13 +3,11 @@ package com.microservices.microservices.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.microservices.entities.Pessoa;
-import com.microservices.microservices.repository.PessoaRepository;
 import com.microservices.microservices.service.PessoaService;
 
 import org.springframework.web.bind.annotation.PostMapping;
